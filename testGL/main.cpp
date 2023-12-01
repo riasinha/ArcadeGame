26a8817b3db9f012a12db1c1d4fbc509e84999b8
riasinha <67247334+riasinha@users.noreply.github.com>
2023-11-30 17:47:50 -0800

    updated maze + added ghosts

--- testGL/main.cpp
+++ testGL/main.cpp
@@ -1,57 +1,124 @@
-#include "dependencies/include/GLFW/glfw3.h"
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
-// Define the maze grid size based on your image (this should match your maze's dimensions)
-const int MAZE_WIDTH = 10;
-const int MAZE_HEIGHT = 10;
+// Maze configuration
+const int MAZE_WIDTH = 15;
+const int MAZE_HEIGHT = 15;
+std::vector<std::vector<int>> maze;
 
-// This is a placeholder for where you would load your image and process it to a maze
-// The actual implementation of image loading and processing is not provided
-std::vector<std::vector<int> > maze(MAZE_HEIGHT, std::vector<int>(MAZE_WIDTH));
+// Window configuration
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// Character configuration
+float characterSize = 0.1f; // The size of the character
+float characterX = 1.5f; // The initial X position of the character
+float characterY = 1.5f; // The initial Y position of the character
+float movementSpeed = 0.05f; // Adjust the movement speed as needed
+
+// Ghost configuration
+struct Ghost {
+    float x, y; // Position
+    float radius; // Radius of the ghost circle
+    float r, g, b; // Color
+} ghosts[] = {
+    {3.5f, 3.5f, 0.2f, 1.0f, 0.0f, 0.0f}, // Red ghost
+    {5.5f, 5.5f, 0.2f, 1.0f, 0.0f, 1.0f}, // Purple ghost
+    {7.5f, 7.5f, 0.2f, 0.0f, 1.0f, 1.0f}  // Turquoise ghost
+};
 
 void initializeMaze() {
-    int rawMaze[MAZE_HEIGHT][MAZE_WIDTH] = {
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 0, 0, 0, 1, 0, 1, 0, 0, 1},
-        {1, 0, 1, 0, 1, 0, 1, 0, 1, 1},
-        {1, 0, 1, 0, 0, 0, 1, 0, 0, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-        {1, 1, 1, 1, 1, 1, 0, 1, 0, 1},
-        {1, 0, 0, 0, 0, 1, 0, 0, 0, 1},
-        {1, 1, 1, 1, 0, 1, 1, 1, 0, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    maze = {
+        {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+        {2, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+        {2, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 2},
+        {2, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 2},
+        {2, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 2},
+        {2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2},
+        {2, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 2},
+        {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+        {2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2},
+        {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+        {2, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2},
+        {2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2},
+        {2, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2},
+        {2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4, 2},
+        {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
     };
-
-    for (int y = 0; y < MAZE_HEIGHT; ++y) {
-        for (int x = 0; x < MAZE_WIDTH; ++x) {
-            maze[y][x] = rawMaze[y][x];
-        }
-    }
 }
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+        characterX -= movementSpeed;
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        characterX += movementSpeed;
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+        characterY += movementSpeed;
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        characterY -= movementSpeed;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void drawCharacter() {
+    glColor3f(0.53f, 0.81f, 0.98f); // Character color
+
+    glBegin(GL_QUADS);
+        glVertex2f(characterX - characterSize, characterY - characterSize); // bottom-left
+        glVertex2f(characterX + characterSize, characterY - characterSize); // bottom-right
+        glVertex2f(characterX + characterSize, characterY + characterSize); // top-right
+        glVertex2f(characterX - characterSize, characterY + characterSize); // top-left
+    glEnd();
+}
+
+void drawGhost(Ghost ghost) {
+    const int numSegments = 16; // Number of segments used to draw the circle
+
+    glColor3f(ghost.r, ghost.g, ghost.b); // Ghost color
+    glBegin(GL_TRIANGLE_FAN);
+        glVertex2f(ghost.x, ghost.y); // Center of circle
+        for (int i = 0; i <= numSegments; i++) { // Last vertex same as first vertex
+            float theta = 2.0f * 3.1415926f * float(i) / float(numSegments); // Current angle
+            float x = ghost.radius * cosf(theta); // Calculate the x component
+            float y = ghost.radius * sinf(theta); // Calculate the y component
+            glVertex2f(x + ghost.x, y + ghost.y);
+        }
+    glEnd();
+}
+
 
 void drawMaze() {
     for (int y = 0; y < MAZE_HEIGHT; ++y) {
         for (int x = 0; x < MAZE_WIDTH; ++x) {
             if (maze[y][x] == 1) {
-                // Draw a wall
-                glColor3f(0.0f, 0.0f, 0.0f); // Black color for walls
+                glColor3f(0.0f, 0.0f, 1.0f); // Blue color for walls
+            } else if (maze[y][x] == 2) {
+                glColor3f(0.0f, 0.0f, 0.0f); // Black color for the border
+            } else if (maze[y][x] == 3) {
+                glColor3f(1.0f, 1.0f, 0.0f); // Yellow color for the start
+            } else if (maze[y][x] == 4) {
+                glColor3f(0.0f, 1.0f, 0.0f); // Green color for the end
             } else {
-                // Draw a path
                 glColor3f(1.0f, 1.0f, 1.0f); // White color for paths
             }
 
-            // Draw a quad at the (x, y) position
             glBegin(GL_QUADS);
-                glVertex2i(x, y);
-                glVertex2i(x + 1, y);
-                glVertex2i(x + 1, y + 1);
-                glVertex2i(x, y + 1);
+                glVertex2f(x, y);
+                glVertex2f(x + 1, y);
+                glVertex2f(x + 1, y + 1);
+                glVertex2f(x, y + 1);
             glEnd();
         }
     }
 }
+
 
 int main() {
     if (!glfwInit()) {
@@ -59,34 +126,39 @@ int main() {
         return -1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Simple Maze Renderer", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Maze Game", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(window);
-    initializeMaze();
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Set up an orthogonal projection matrix
+    initializeMaze(); // Populate the maze data
+
     glMatrixMode(GL_PROJECTION);
-    glLoadIdentity(); // Reset the projection to the identity matrix
-    glOrtho(0.0, double(MAZE_WIDTH), double(MAZE_HEIGHT), 0.0, -1.0, 1.0);
+    glLoadIdentity();
+    glOrtho(0.0, MAZE_WIDTH, 0.0, MAZE_HEIGHT, -1.0, 1.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
     while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         drawMaze();
+        drawCharacter();
+        for (Ghost &ghost : ghosts) {
+            drawGhost(ghost);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
-
